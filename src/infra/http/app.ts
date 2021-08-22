7c47a440600c6075232569839666cf65e7024b2e
Lucas Ferreira <lucsferreira.dev@gmail.com>
2021-08-22 00:06:20 -0400

    Initial commit

---
+++ src/infra/http/app.ts
@@ -0,0 +1,8 @@
+import 'reflect-metadata'
+import 'express-async-errors'
+
+import express from 'express'
+
+const app = express()
+
+export { app }
---
+++ src/server.ts
@@ -0,0 +1,3 @@
+import { app } from './infra/http/app'
+
+app.listen(8000, () => console.log('Server listening at port 8000'))
